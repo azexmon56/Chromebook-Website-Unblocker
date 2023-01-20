7c9723478ff09e3551e9bed4e5e01db939e358a7
azexmon56 <102878815+azexmon56@users.noreply.github.com>
2023-01-19 20:58:07 -0600

    Update Website.py

--- Website.py
+++ Website.py
@@ -1,10 +1,7 @@
 import requests, sys, webbrowser, bs4, os, time
 
 
-link_list = [
-"https://yahoo.com"
-]
 
 print('Openning list of websites...')
 
-webpage = webbrowser.open(link)
+webpage = webbrowser.open("https://yahoo.com")
